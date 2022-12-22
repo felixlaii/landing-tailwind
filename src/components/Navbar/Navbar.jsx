0a886095353f3b2b9be04a46afc6281646ed07e9
Felix Lai <felix.lai@hotmail.com>
2022-12-22 13:54:58 -0500

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Navbar = () => {
-    const [nav, setNav] = useState(false);
+  const [nav, setNav] = useState(false);
 
-    const handleNav = () => {
-        setNav(!nav);
-      };
+  const handleNav = () => {
+    setNav(!nav);
+  };
 
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+      <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
+      <ul className="hidden md:flex"></ul>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
